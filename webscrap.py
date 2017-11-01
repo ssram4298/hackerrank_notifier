f146faab5e8109513a7c91557762cb6ba9bc6d24
Sai Sri Ram Ampapurapu <30355244+ssram4298@users.noreply.github.com>
2017-11-01 12:02:50 +0530

    updated notify-send
    Notify-send will work  in windows for python 2 versions

--- webscrap.py
+++ webscrap.py
@@ -14,13 +14,13 @@ a = soup.find_all("ul" , {"class": "contests-active"})
 b = a[0].find_all("li")
 for i in range(len(b)): 
     c = b[i].div.find_all("div", {"class": "contest-name"})
-    print(c[0].get_text())
+    print c[0].get_text()
     contest_name.append(c[0].get_text())
 
-print()
+print
 
 d = b[0].div.find_all("div", {"class": "contest-status"})
-print(d[0].get_text())
+print d[0].get_text()
 end_date.append(d[0].get_text())
 
 for i in range(1,len(b)): 
@@ -28,9 +28,8 @@ for i in range(1,len(b)):
     e = d[0].find_all("meta", {"itemprop": "endDate"})
     date_posted = e[0]['content']
     last_date = datetime.datetime.strptime(date_posted, '%Y-%m-%dT%H:%M:%S.%fZ')
-    print(last_date)
+    print last_date
     end_date.append(last_date)
 
 for t in range(len(contest_name)):
-    call(["notify-send",contest_name[t],"\n",'End Date:'+end_date[t]])    
-    #call(["notify-send",A[t],"\n",B[t],"\n",'Start Date:'+C[t],"\n",'End Date:'+D[t]])
+    call(["notify-send",contest_name[t],"\n",'End Date:'+str(end_date[t])]) 
